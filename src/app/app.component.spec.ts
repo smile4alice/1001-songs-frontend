c6091e68f1a6474918dd4d0472beb3cc4f718591
baza-trainee <baza.trainee@gmail.com>
2023-06-19 21:36:53 +0300

    test2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('1001-songs-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('1001-songs-frontend app is running!');
-  });
 });
