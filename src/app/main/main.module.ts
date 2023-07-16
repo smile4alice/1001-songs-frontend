539c992436c0495fb7c96eafb10e6a4eb7da08b4
baza-trainee <baza.trainee@gmail.com>
2023-07-16 14:02:50 +0300

    some fixed

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { AboutComponent } from './component/about/about.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './component/home/home-routing.module';
 
 @NgModule({
    declarations: [
-      MainComponent,
-      AboutComponent
+      MainComponent
     ],
    imports: [
       SharedModule,
-      HomeRoutingModule,
-      AppRoutingModule
+      HomeRoutingModule
    ],
    exports: [
       MainComponent
