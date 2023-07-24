ba67d95a28a62f40161975d5ce702db86b05e9ae
Yuliia90 <0638837520izi@gmail.com>
2023-07-24 14:01:59 +0300

    -translate of home-page -fix adaptive button rules(width) -fix constants

--- src/app/main/component/home/home.component.ts
+++ src/app/main/component/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 import {Homepage} from "../../../shared/constants/constants";
-import { RouterLink } from '@angular/router';
+import {RouterLink} from "@angular/router";
+
 
 
 @Component({
@@ -9,16 +10,12 @@ import { RouterLink } from '@angular/router';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-    imports: [RouterLink]
+    imports: [TranslateModule, RouterLink]
 })
 export class HomeComponent {
-  protected readonly HomepageShare = Homepage;
-  protected readonly HomepageDonate = Homepage;
-  protected readonly HomepageFlag = Homepage;
+  protected readonly homePageAssets = Homepage;
 
   constructor(
     private _translate: TranslateService){}
-
-
 
 }
