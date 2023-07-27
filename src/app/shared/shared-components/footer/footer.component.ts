ae91c8653c743cf72b66b3c1bb6fe70407c27e37
AlexW8719 <vynnyk87@gmail.com>
2023-07-27 14:11:42 +0300

    footer

--- src/app/shared/shared-components/footer/footer.component.ts
+++ src/app/shared/shared-components/footer/footer.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { navLinksHeader } from '../../enums/navLinks.enum';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
     standalone: true,
-    imports: [TranslateModule]
+    imports: [TranslateModule, CommonModule, RouterLink, RouterLinkActive]
 })
 export class FooterComponent {
+  public navLinks = navLinksHeader;
 
   constructor(
     private _translate: TranslateService
