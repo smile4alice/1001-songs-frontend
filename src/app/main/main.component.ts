f94e4c433befd5c8f49325e9ba2ecac53ec62d75
Yuliia90 <0638837520izi@gmail.com>
2023-08-03 16:43:07 +0300

    -fix adaptive home-page and error page -create home-tape page -translate home-tape page -adaptive home-tape page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/shared-components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../shared/shared-components/header/header.component';
+import {HomeTapeComponent} from "../shared/shared-components/home-tape/home-tape.component";
 
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
     standalone: true,
-    imports: [HeaderComponent, RouterOutlet, FooterComponent]
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, HomeTapeComponent]
 })
 export class MainComponent {
 
--- src/app/shared/enums/icons.enum.ts
+++ src/app/shared/enums/icons.enum.ts
@@ -2,5 +2,6 @@ const path = 'assets/img/home/icons/'
 
 export enum Homepage {
   donateIcon = `${path}volunteer_activism.svg`,
-  flagIcon = `${path}flag.svg`
+  flagIcon = `${path}flag.svg`,
+  arrowsTape = `${path}arrows_tape.svg`
 }
