ffbb7148450352aec325f8b71dbbc69055e8315b
Yuliia90 <0638837520izi@gmail.com>
2023-08-03 22:10:19 +0300

    -fix bug adaptive home-tape page

--- src/app/main/component/home/home.component.ts
+++ src/app/main/component/home/home.component.ts
@@ -4,6 +4,7 @@ import {RouterLink} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {Homepage} from "../../../shared/enums/icons.enum";
 import {ErrorComponent} from "../../../shared/shared-components/error/error.component";
+import {HomeTapeComponent} from "../../../shared/shared-components/home-tape/home-tape.component";
 
 
 @Component({
@@ -11,7 +12,7 @@ import {ErrorComponent} from "../../../shared/shared-components/error/error.comp
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-  imports: [RouterLink, TranslateModule, CommonModule, ErrorComponent]
+  imports: [RouterLink, TranslateModule, CommonModule, ErrorComponent, HomeTapeComponent]
 })
 export class HomeComponent {
   protected readonly homePageAssets = Homepage;
