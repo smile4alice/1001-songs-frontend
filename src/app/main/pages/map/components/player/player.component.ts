b4ac78e81dab144ea552fb1d4ee864a477fcf56e
Olga Shutyleva <99029912+shytuleva24@users.noreply.github.com>
2024-01-18 11:50:40 +0100

    added pagination to the map and education pages (#143)
    * added pagination to the map page
    
    * added pagination to the map page and education

--- src/app/main/pages/map/components/player/player.component.ts
+++ src/app/main/pages/map/components/player/player.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {Observable, Subscription} from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+
 import { StereoPlayerComponent } from './stereo-player/stereo-player.component';
 import { MultichanelPlayerComponent } from './multichanel-player/multichanel-player.component';
-import { Observable } from 'rxjs';
 import { PlaylistSongCardComponent } from './playlist-song-card/playlist-song-card.component';
-import { Select, Store } from '@ngxs/store';
 import { Song } from 'src/app/shared/interfaces/song.interface';
 import { PlayerState } from 'src/app/store/player/player.state';
+import {PaginationComponent} from "../../../../../shared/shared-components/pagination/pagination.component";
 
 @Component({
   selector: 'app-player',
@@ -20,25 +22,57 @@ import { PlayerState } from 'src/app/store/player/player.state';
     TranslateModule,
     StereoPlayerComponent,
     MultichanelPlayerComponent,
-    PlaylistSongCardComponent
+    PlaylistSongCardComponent,
+    PaginationComponent
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnDestroy{
   screenWidth: number = 0;
   serverStaticImgPath: string = './assets/img/player/';
   staticVideoImgUrl: string = './assets/img/player/video_mock.png';
+  public itemsPerPage: number = 10;
+  public currentPage: number = 1;
+  songs: Song[] = [];
+  private readonly subscription?: Subscription;
 
-  @Select(PlayerState.getSongs) songs$?: Observable<Song[]>;
+
+  @Select(PlayerState.getSongs) songs$!: Observable<Song[]>;
   @Select(PlayerState.getSelectedSong) selectedSong$?: Observable<Song>;
 
   location = 'Ромейки';
 
   constructor(
     private _translate: TranslateService,
-
     private store: Store
-  ) {}
+  ) {
+    this.subscription = this.songs$.subscribe((data) => {
+      if (data) this.songs = data.slice();
+    });
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.songs.length / this.itemsPerPage);
+  }
+
+  get itemsOnCurrentPage(): Song[] {
+    if (this.songs.length <= this.itemsPerPage) return this.songs;
+
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+
+    return this.songs.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
--- src/app/main/pages/science/components/pages/science-songs/science-songs.component.ts
+++ src/app/main/pages/science/components/pages/science-songs/science-songs.component.ts
@@ -1,54 +1,94 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {BreadcrumbsComponent} from "../../../../../../shared/shared-components/breadcrumbs/breadcrumbs.component";
-import {Observable, take} from "rxjs";
+import {Observable, Subscription, take} from "rxjs";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Select, Store} from "@ngxs/store";
+import {TranslateModule} from "@ngx-translate/core";
+
+
 import {
   MultichanelPlayerComponent
 } from "../../../../map/components/player/multichanel-player/multichanel-player.component";
+import {BreadcrumbsComponent} from "../../../../../../shared/shared-components/breadcrumbs/breadcrumbs.component";
 import {StereoPlayerComponent} from "../../../../map/components/player/stereo-player/stereo-player.component";
 import {
   PlaylistSongCardComponent
 } from "../../../../map/components/player/playlist-song-card/playlist-song-card.component";
-import {Select, Store} from "@ngxs/store";
 import {PlayerState} from "../../../../../../store/player/player.state";
 import {Song} from "../../../../../../shared/interfaces/song.interface";
 import {FetchSongsByLocation} from "../../../../../../store/player/player.actions";
-import {TranslateModule} from "@ngx-translate/core";
 import {scienceCategories} from "../../../../../../static-data/categoriesList";
 import {ImageSliderComponent} from "../../shared-components/image-slider/image-slider.component";
+import {PaginationComponent} from "../../../../../../shared/shared-components/pagination/pagination.component";
 
 @Component({
   selector: 'app-science-songs',
   standalone: true,
-  imports: [CommonModule, BreadcrumbsComponent, MultichanelPlayerComponent, StereoPlayerComponent, PlaylistSongCardComponent, TranslateModule, ImageSliderComponent],
+  imports: [
+    CommonModule,
+    BreadcrumbsComponent,
+    MultichanelPlayerComponent,
+    StereoPlayerComponent,
+    PlaylistSongCardComponent,
+    TranslateModule,
+    ImageSliderComponent,
+    PaginationComponent
+  ],
   templateUrl: './science-songs.component.html',
   styleUrls: ['./science-songs.component.scss']
 })
-export class ScienceSongsComponent implements OnInit {
-  @Select(PlayerState.getSongs) songs$?: Observable<Song[]>;
-
+export class ScienceSongsComponent implements OnInit, OnDestroy {
+  @Select(PlayerState.getSongs) songs$!: Observable<Song[]>;
+  public itemsPerPage: number = 10;
+  public currentPage: number = 1;
+  songs: Song[] = [];
+  private readonly subscription?: Subscription;
   title!: string;
-  about1: string = "<p>Діти (віком від п’яти до десяти/дванадцяти років) колядували/щедрували поодинці або невеличкими групками. Хлопчики і дівчатка могли ходити разом. Найчастіше бігали до родичів, сусідів, хрещених.</p> <p>У текстах дитячих колядок і щедрівок переважають мотиви випрошування дарів та ритуальні погрози господарям.</p> <p>Колядки/щедрівки дитячого репертуару мають вузький діапазон (секунда, терція, кварта). Пісні засновані на безкінечному повторі однієї (рідше двох) поспівок. Текст при цьому змінюється, рефрени дитячим пісням не властиві.</p>";
-  about2: string = "<p>Зазвичай діти інтонують приблизно, спів часто переходить у скандування без чіткої висоти. Натомість ритм (три базові ритмічні формули) діти відтворюють чітко (див. на картах)</p>";
+  about1: string =
+    '<p>Діти (віком від п’яти до десяти/дванадцяти років) колядували/щедрували поодинці або невеличкими групками. Хлопчики і дівчатка могли ходити разом. Найчастіше бігали до родичів, сусідів, хрещених.</p> <p>У текстах дитячих колядок і щедрівок переважають мотиви випрошування дарів та ритуальні погрози господарям.</p> <p>Колядки/щедрівки дитячого репертуару мають вузький діапазон (секунда, терція, кварта). Пісні засновані на безкінечному повторі однієї (рідше двох) поспівок. Текст при цьому змінюється, рефрени дитячим пісням не властиві.</p>';
+  about2: string =
+    '<p>Зазвичай діти інтонують приблизно, спів часто переходить у скандування без чіткої висоти. Натомість ритм (три базові ритмічні формули) діти відтворюють чітко (див. на картах)</p>';
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private store: Store
-  ) {}
-
-  ngOnInit(): void {
-    this.store.dispatch(new FetchSongsByLocation("Ромейки"));
-    this.route.params.pipe(take(1)).subscribe(params => {
-      const category = scienceCategories.find(category => category.routerLink === params['category']);
-      const subCategory = category?.genreGroups
-        .flatMap(group => group.subCategories)
-        .find(sub => sub.query === params['id']);
-
-      subCategory?.title ? this.title = subCategory.title : this.router.navigate(['/404']);
-
+  ) {
+    this.subscription = this.songs$.subscribe((data) => {
+      if (data) this.songs = data.slice();
     });
   }
 
+  get totalPages(): number {
+    return Math.ceil(this.songs.length / this.itemsPerPage);
+  }
+
+  get itemsOnCurrentPage(): Song[] {
+    if (this.songs.length <= this.itemsPerPage) return this.songs;
+
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+
+    return this.songs.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(new FetchSongsByLocation('Ромейки'));
+    this.route.params.pipe(take(1)).subscribe((params) => {
+      const category = scienceCategories.find((category) => category.routerLink === params['category']);
+      const subCategory = category?.genreGroups.flatMap((group) => group.subCategories).find((sub) => sub.query === params['id']);
+
+      subCategory?.title ? (this.title = subCategory.title) : this.router.navigate(['/404']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
