cdfc70a4b176a3fe98c2d7ebb9fad062cae807e6
Olga Shutyleva <99029912+shytuleva24@users.noreply.github.com>
2024-03-04 12:41:19 +0100

    implement a pop-up development team (#215)
    * fix right slider
    
    * add photo in science cycle
    
    * redact team-popup
    
    * test fix
    
    * test fix
    
    * add border radius
    
    * add team-popup
    
    * fix test
    
    * fix bug

--- src/app/shared/shared-components/footer/team-popup/team-popup.component.ts
+++ src/app/shared/shared-components/footer/team-popup/team-popup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-team-popup',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, MatDialogModule],
   templateUrl: './team-popup.component.html',
   styleUrls: ['./team-popup.component.scss']
 })
