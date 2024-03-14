91a1480591a9f28af50ab9072496d5b86d46669c
Yurii <yurikolechkin@gmail.com>
2024-03-14 12:06:15 +0200

    fix: now filter form values clear on destroy (#244)
    * fix: now filter form values clear on destroy
    
    * fix: remove unused import

--- src/app/main/pages/map/components/map-filter/map-filter.component.ts
+++ src/app/main/pages/map/components/map-filter/map-filter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { combineLatestWith, debounceTime, distinctUntilChanged, filter, map, Observable, skip, Subject, takeUntil, tap } from 'rxjs';
+import { combineLatestWith, debounceTime, distinctUntilChanged, filter, map, Observable, Subject, takeUntil, tap } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
@@ -55,7 +55,7 @@ export class MapFilterComponent implements OnInit, OnDestroy {
 
     this.form?.valueChanges
       .pipe(distinctUntilChanged((prev,curr)=> prev.title === curr.title))
-      .pipe(skip(1))
+     // .pipe(skip(1))
       .pipe(
         tap((values) => {
           if (values.title === '') {
@@ -67,9 +67,9 @@ export class MapFilterComponent implements OnInit, OnDestroy {
         })
       )
       .pipe(map((filter) => filter.title))
-      .pipe(takeUntil(this.destroy$))
       .pipe(combineLatestWith(this.songs))
       .pipe(debounceTime(300))
+      .pipe(takeUntil(this.destroy$))
       .pipe(
         distinctUntilChanged((p, c) => {
           const [pSearch, pSongs] = p;
@@ -149,6 +149,7 @@ export class MapFilterComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.form.setValue(new SongFilter());
     this.destroy$.next();
     this.destroy$.complete();
   }
