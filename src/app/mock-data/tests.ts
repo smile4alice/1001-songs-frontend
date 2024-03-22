163db19e0fde60a815f9f5616e374e7449d5c30f
Yurii <yurikolechkin@gmail.com>
2024-03-22 18:41:52 +0200

    Fe/fix og meta title (#290)
    * fix: player now dont pause after pagination
    
    * update: stereoplayer volume control functionality
    
    * fix: volume control behavior after redirection
    
    * update: volume control on multichannel player
    
    * fix: lint notes
    
    * update: og title
    
    * change : addtags meta service method to the update tag ones
    
    * fix: remove meta og from about component
    
    * fix : title in the index html
    
    * chore: remove unused files

--- src/app/mock-data/tests.ts
+++
@@ -1,9 +0,0 @@
-export const testCategories: string[] = [
-  'expeditions.categories.all',
-  'expeditions.categories.exploring',
-  'expeditions.categories.static',
-  'expeditions.categories.interdisciplinary',
-  'expeditions.categories.thematic',
-  'expeditions.categories.video-of-ritual',
-  'expeditions.categories.digital-rcord'
-];
